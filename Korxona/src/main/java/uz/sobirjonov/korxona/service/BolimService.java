93d16151800189460ea92bf21ed70abeab39ef06
SOBIRJONOVASADBEK <152601144+SOBIRJONOVASADBEK@users.noreply.github.com>
2023-12-01 23:34:56 +0500

    Update BolimService.java

--- Korxona/src/main/java/uz/sobirjonov/korxona/service/BolimService.java
+++ Korxona/src/main/java/uz/sobirjonov/korxona/service/BolimService.java
@@ -1,4 +1,19 @@
 package uz.sobirjonov.korxona.service;
 
 public class BolimService {
+  private final BolimRepository bolimRepository;
+
+    public BolimService(BolimRepository bolimRepository) {
+        this.bolimRepository = bolimRepository;
+    }
+    public Bolim add(Bolim bolim){
+        return bolimRepository.save(bolim);
+    }
+    public void delete(Long id){
+       Bolim bolim=bolimRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Xodim with ID " + id + " not found"));
+       bolimRepository.delete(bolim);
+
+    }
+
 }
