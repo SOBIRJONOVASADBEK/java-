c87dd9c169ce6e7111480d3b673e5c13c99d01c5
SOBIRJONOVASADBEK <152601144+SOBIRJONOVASADBEK@users.noreply.github.com>
2023-12-01 23:35:36 +0500

    Update BolimController.java

--- Korxona/src/main/java/uz/sobirjonov/korxona/controller/BolimController.java
+++ Korxona/src/main/java/uz/sobirjonov/korxona/controller/BolimController.java
@@ -1,4 +1,20 @@
 package uz.sobirjonov.korxona.controller;
-
+@RestController
+@RequestMapping("/bolim")
 public class BolimController {
+   private final BolimService bolimService;
+
+    public BolimController(BolimService bolimService) {
+        this.bolimService = bolimService;
+    }
+    @PostMapping("/save")
+    public ResponseEntity<Bolim> addBolim(@RequestBody Bolim bolim){
+        Bolim result=bolimService.add(bolim);
+        return ResponseEntity.ok(result);
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(@RequestParam Long id){
+        bolimService.delete(id);
+        return ResponseEntity.ok("Bo'lim o'chirildi");
+    }
 }
